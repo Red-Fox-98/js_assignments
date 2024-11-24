8aff70bc70365ef624c0453012e45c7e239786c2
Red-Fox-98 <evgeniya.zaitseva.98@gmail.com>
2024-11-24 13:28:22 +0300

    Added tests

--- task4/library.test.js
+++ task4/library.test.js
@@ -9,14 +9,26 @@ test("addBook", () => {
     expect(lib.books.length).toBe(0);
 
     lib.addBook(book1);
-    expect(lib.books.length).toBe(1);
     expect(lib.books[0]).toBe(book1);
 
     lib.addBook(book2);
-    expect(lib.books.length).toBe(2);
     expect(lib.books[1]).toBe(book2);
 
     expect(() => {
         lib.addBook(book2);
     }).toThrow();
+})
+
+test("borrowBook", () => {
+    lib.borrowBook(7843672087);
+    expect(lib.listAvailableBooks().length).toBe(1);
+})
+
+test("returnBook", () => {
+    lib.returnBook(7843672087);
+    expect(lib.listAvailableBooks().length).toBe(2);
+})
+
+test("listAvailableBooksBook", () => {
+    expect(lib.listAvailableBooks().length).toBe(lib.books.filter((book) => book.isAvailable).length);
 })
